28eb0f88846ae4f46279ee5901b499ad1ef35647
Gerard Moya Tarradellas <gmoyaapps@gmail.com>
2021-09-21 19:57:57 +0200

    16 - Adding our first component

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateComponent implements OnInit {
+  /** Constructor de la clase */
+  constructor() {}
+
+  /** Se llama cada vez que el componente se inicializa */
+  ngOnInit(): void {}
+}
