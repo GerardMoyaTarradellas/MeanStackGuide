7e7042b6bca2c86627273feb5a8e1986cad1d296
Gerard Moya Tarradellas <gmoyaapps@gmail.com>
2021-10-05 19:34:39 +0200

    38 - Fetching initial posts

--- back-end/app.js
+++ back-end/app.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use((request, response, next) => {
-  console.log("First");
-  next();
-});
-
-app.use((request, response, next) => {
-  console.log("Second");
-  response.send("Hello from express");
+app.use("/api/posts", (request, response, next) => {
+  const posts = [
+    { _id: "0", title: "Titulo 1", content: "Contenido 1" },
+    { _id: "1", title: "Titulo 2", content: "Contenido 2" },
+    { _id: "2", title: "Titulo 3", content: "Contenido 3" },
+  ];
+  response.status(200).json({
+    message: "Posts fetch succesfully",
+    posts: posts,
+  });
 });
 
 module.exports = app;
