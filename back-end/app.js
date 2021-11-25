a5be8c09dff05b7cf18226823c93f21529d4f795
Gerard Moya Tarradellas <gmoyaapps@gmail.com>
2021-11-25 19:56:12 +0100

    52 - Creating a post instance

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const Post = require("./models/post");
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//rPwswr21jZVN536A;
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,9 +24,13 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  console.log(req.body);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  console.log(post);
   res.status(201).json({
-    message: "Post created!",
+    message: "Post creado!",
   });
 });
 
