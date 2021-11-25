f188efc72c6d163b5d400fa3b6a3d5f8ba5f62a6
Gerard Moya Tarradellas <gmoyaapps@gmail.com>
2021-11-25 20:19:45 +0100

    55 - Fetching data from a Database

--- back-end/app.js
+++ back-end/app.js
@@ -45,14 +45,11 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    { _id: "0", title: "Titulo 1", content: "Contenido 1" },
-    { _id: "1", title: "Titulo 2", content: "Contenido 2" },
-    { _id: "2", title: "Titulo 3", content: "Contenido 3" },
-  ];
-  res.status(200).json({
-    message: "Posts fetch succesfully",
-    posts: posts,
+  Post.find().then((posts) => {
+    res.status(200).json({
+      message: "Posts fetch succesfully",
+      posts: posts,
+    });
   });
 });
 
