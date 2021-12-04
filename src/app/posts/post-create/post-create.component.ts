bc181ecfc0cf850cdbea1970c459d97972346bb6
Gerard Moya Tarradellas <gmoyaapps@gmail.com>
2021-12-04 09:47:03 +0100

    75 - Adding image controls to store the image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -68,7 +68,22 @@ export class PostCreateComponent implements OnInit {
       content: new FormControl(null, {
         validators: [Validators.required],
       }),
+      image: new FormControl(null, {
+        validators: [Validators.required],
+      }),
     });
+  }
+
+  /**
+   * Se ejecuta cada vez que se cambia el archivo seleccionado
+   * @param event Evento recogido
+   */
+  public onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    console.log(this.form);
   }
 
   /**
