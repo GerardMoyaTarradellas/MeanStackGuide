323e67c27f45b112c270321c40790ae030c2f551
Gerard Moya Tarradellas <gmoyaapps@gmail.com>
2021-12-04 12:18:49 +0100

    98 - Creating the user model

--- back-end/app.js
+++ back-end/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const post_router = require("./routes/posts");
+const user_routes = require("./routes/user");
 
 const app = express();
 
@@ -36,5 +37,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", post_router);
+app.use("/api/user", user_routes);
 
 module.exports = app;
---
+++ back-end/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const unique_validator = require("mongoose-unique-validator");
+
+/** Esquema del `Post` */
+const UserSchema = mongoose.Schema({
+  /** Email del usuario. */
+  title: { type: String, required: true, unique: true },
+  /** Email del usuario. */
+  password: { type: String, required: true },
+});
+
+UserSchema.plugin(unique_validator);
+
+module.exports = mongoose.model("User", UserSchema);
