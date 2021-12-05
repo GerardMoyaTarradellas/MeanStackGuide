4baf9023e6883c3e944ce0183787a89d9b9a83fb
Gerard Moya Tarradellas <gmoyaapps@gmail.com>
2021-12-05 19:12:55 +0100

    115 - Adding a reference to the model

--- back-end/models/post.js
+++ back-end/models/post.js
@@ -8,6 +8,12 @@ const PostSchema = mongoose.Schema({
   content: { type: String, required: true },
   /** Path de la imagen. */
   image_path: { type: String, required: true },
+  /** Creador del post. */
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
