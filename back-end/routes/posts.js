ba46ce8b229827a0a70c6d729c290dd4c3a3b15b
Gerard Moya Tarradellas <gmoyaapps@gmail.com>
2021-12-06 17:23:09 +0100

    131 - Separating the middleware

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -1,41 +1,14 @@
 const express = require("express");
-const multer = require("multer");
 
 const PostController = require("../controllers/posts");
 
 const auth = require("../middleware/check-auth");
+const extractFile = require("../middleware/multer");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpeg",
-  "image/jpg": "jpg",
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    const is_valid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Error en el Mime type");
-    if (is_valid) {
-      error = null;
-    }
-    callback(null, "back-end/images");
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.toLowerCase().split(" ").join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    callback(null, name + "-" + Date.now() + "." + ext);
-  },
-});
-
 /** Generación de posts. */
-router.post(
-  "",
-  auth,
-  multer({ storage: storage }).single("image"),
-  PostController.createPost
-);
+router.post("", auth, extractFile, PostController.createPost);
 
 /** Consulta de posts existentes en la db. */
 router.get("", PostController.getAllPosts);
@@ -47,11 +20,6 @@ router.get("/:id", PostController.getOnePost);
 router.delete("/:id", auth, PostController.deleteOne);
 
 /** Eliminación de un post */
-router.put(
-  "/:id",
-  auth,
-  multer({ storage: storage }).single("image"),
-  PostController.editPost
-);
+router.put("/:id", auth, extractFile, PostController.editPost);
 
 module.exports = router;
